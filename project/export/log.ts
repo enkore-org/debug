73605f408cd32202c9c96ed5c48af6bdaa57e89a
eXory2024 <exory@anio.software>
2025-06-20 13:59:07 +0200

    Use #~src/__logImplementation.ts.

--- project/export/log.ts
+++ project/export/log.ts
@@ -1,4 +1,4 @@
-import {logImplementation} from "#~embeds/log.ts"
+import {logImplementation} from "#~src/__logImplementation.ts"
 
 export function log(message: string) {
 	logImplementation(message)
